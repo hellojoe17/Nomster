4dc712162d3d2e8faaf265d5edb0f5574fbd0221
Joe Maldonado <jmaldonado1329@gmail.com>
2014-10-18 03:36:50 +0000

    fixed allignment of table on places page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -27,6 +27,14 @@ class PlacesController < ApplicationController
 		redirect_to root_path
 	end
 
+	def destroy
+		@place = Place.find(params[:id])
+		@place.destroy
+		redirect_to root)path
+	end
+
+		
+	end
 	private
 
 	def place_params
