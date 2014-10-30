8a5cd447db26207b99c5109af1c4184cc08576eb
Joe Maldonado <jmaldonado1329@gmail.com>
2014-10-30 13:31:30 +0000

    added a form for the individual photos and created photos database

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,6 +4,7 @@ class Place < ActiveRecord::Base
 	paginates_per 4
 	geocoded_by :address
 	after_validation :geocode
+	has_many :photos
 	
 	validates :name, :presence => true, length: { minimum: 3 }
 	validates :address, :presence => true
--- db/migrate/20141029171103_create_photos.rb
+++ db/migrate/20141029171103_create_photos.rb
@@ -3,6 +3,7 @@ class CreatePhotos < ActiveRecord::Migration
     create_table :photos do |t|
 		t.string :caption
 		t.string :place_id
+		
 		t.timestamps
     end
   end
