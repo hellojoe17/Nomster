1f48a6410e89a7d933cb16b274e7983678e910c8
Joe Maldonado <jmaldonado1329@gmail.com>
2014-11-04 19:03:52 +0000

    added additional code to upload photos for places

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141029171103) do
+ActiveRecord::Schema.define(version: 20141104183312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20141029171103) do
     t.string   "place_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "Picture"
   end
 
   create_table "places", force: true do |t|
